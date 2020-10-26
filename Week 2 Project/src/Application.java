2453a37da311a2be566d8269f04db369b3d0be18
John <Jeule590@students.ccsnh.edu>
2020-10-26 14:25:16 -0400

    next commit

---
+++ Week 2 Project/src/Application.java
@@ -0,0 +1,46 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Question 2
+//		boolean hotOutside = true;
+//		boolean isWeekday = false;
+//		boolean hasMoneyInPocket = true;
+		// Question 3
+//		double costOfMilk= 5.00;
+//		double moneyInWallet = 4.50;
+//        int thirstLeval = 5;
+        // Question 4
+		// a.
+     boolean shouldBuyIcecream = true; 
+		double p = 4.50; 
+	boolean hotOutside = true;
+      double moneyInWallet = p;
+       		 if(shouldBuyIcecream = hotOutside && moneyInWallet >= 2)  
+    			  System.out.println("you can buy icecream");
+        //b.
+       boolean notAWeekday = true;
+       boolean willGoSwimming = true;
+       if (willGoSwimming = hotOutside && notAWeekday);
+       System.out.println("I will go swiming");
+       
+       //c.
+       boolean isAGoodDay = true;
+    		   if (isAGoodDay = hotOutside && moneyInWallet>=3 && notAWeekday);
+    		   System.out.println("It is a most excelant day");
+    		
+      //d. 
+      double thirstLevel = 5;
+      double m = 2.25;
+      boolean willBuyMilk = true;
+      
+      if (willBuyMilk = hotOutside && thirstLevel >= 3 && moneyInWallet >= 2*m);
+    		System.out.println("I will buy milk"); 
+    			  
+       
+ 
+	}
+
+}
---
+++ Week 2 Project/src/Loops.java
@@ -0,0 +1,43 @@
+
+public class Loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// 5. a.
+//		int x = 0;
+//		while (x <= 100) {	
+//			System.out.println(x);
+//			x += 2;
+//   }
+// b.
+//		int x = 100;
+//		while (x >= 1) { 	
+//			System.out.println(x);
+//			x -= 2;
+//	}	
+		
+// c.
+//		for (int i=0; i<=100; i ++) {
+//		if (i % 2 == 0) {
+//		System.out.println(i);
+//	}	
+ 
+		
+// d. 
+//          
+		for (int i=1; i<=100; i ++) {
+			 if (i % 1 == 0)  
+				 System.out.println(i);
+			 if (i % 3 == 0)
+				System.out.println(i + "Hello");
+			 if (i % 5 == 0);
+		        System.out.println(i + "World");
+		     if (i % 3 == 0 && i % 5 == 0); 
+		       System.out.println(i + "HelloWorld"); 
+	 			
+	}
+}
+}
+		
+	
+
